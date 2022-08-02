8045f2f6df42b2bbe8761b677caddf3e268853c9
freddyheredia4 <freddyheredia4@gmail.com>
2022-08-02 08:35:41 -0500

    big bang

---
+++ backend-tickets/src/main/java/edu/yavirac/backendtickets/BackendTicketsApplication.java
@@ -0,0 +1,13 @@
+package edu.yavirac.backendtickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTicketsApplication.class, args);
+	}
+
+}
---
+++ backend-tickets/src/test/java/edu/yavirac/backendtickets/BackendTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yavirac.backendtickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
